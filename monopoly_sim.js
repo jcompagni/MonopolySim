4093ab668e78300d5362ccab3901ae8b14ea5788
jruberg <rubergly@gmail.com>
2011-11-20 17:46:39 -0500

    add monopoly_sim.js wireframe

---
+++ monopoly_sim.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+// FIXME: these should come from the page
+var numPlayers = 1,
+	numTurns = 100,
+	money = false;
+
+function runTest(nPlayers, nTurns, mon) {
+	// FIXME: implement. should return a 36-element array.
+}
+
+// FIXME: we should actually run this several times and figure out the
+// distribution of the hits for each space.
+var hits = runTest();
+
+// FIXME: use highchart to display the result data to the user.
+
+});
